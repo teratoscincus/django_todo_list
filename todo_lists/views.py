0f4b79e9625da99b0d5ccfa44ce5a6e8bddcd29e
Max <teratoscincus@hotmail.com>
2022-12-28 20:16:23 +0100

    Add showing of etries with notes on index page

--- todo_lists/views.py
+++ todo_lists/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 
+from . import models
+
 
 def index(request):
     """Displays the index view o the Todo List app."""
-    return render(request, "todo_lists/index.html")
+    entries = models.Entry.objects.all()
+    notes = models.Note.objects.all()
+    context = {
+        "entries": entries,
+        "notes": notes,
+    }
+    return render(request, "todo_lists/index.html", context)

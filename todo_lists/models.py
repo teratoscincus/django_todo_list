d70edb003435bb34cf70d1b3d4153530e7479fee
Max <teratoscincus@hotmail.com>
2022-12-28 13:02:37 +0100

    Create TodoListEntry model and register to admin

--- todo_lists/models.py
+++ todo_lists/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class TodoListEntry(models.Model):
+    heading = models.CharField(max_length=50)
+    text = models.CharField(max_length=200)

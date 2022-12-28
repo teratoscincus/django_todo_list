04e678fad1c00a0720ae3ebf53207dd0433aa3d5
Max <teratoscincus@hotmail.com>
2022-12-28 13:15:04 +0100

    Add and rename models ad register with admin site
    Rename "TodoListEntry" model to "Entry" and add a Note model with a
    foreign key referensing the Entry model.
    The Note model will act as additional notes or sub goals of the Entry in
    the todo list.

--- todo_lists/admin.py
+++ todo_lists/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import TodoListEntry
+from .models import Entry, Note
 
-admin.site.register(TodoListEntry)
+admin.site.register(Entry)
+admin.site.register(Note)
--- todo_lists/models.py
+++ todo_lists/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 
 
-class TodoListEntry(models.Model):
+class Entry(models.Model):
+    heading = models.CharField(max_length=50)
+    text = models.CharField(max_length=200)
+
+
+class Note(models.Model):
+    """Additional notes for the Entry model."""
+
+    parent_entry = models.ForeignKey(Entry, on_delete=models.PROTECT)
     heading = models.CharField(max_length=50)
     text = models.CharField(max_length=200)

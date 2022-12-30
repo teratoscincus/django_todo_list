820b8452481d780b37437a8f041c6d67c14ae439
Max <teratoscincus@hotmail.com>
2022-12-30 22:21:49 +0100

    Only display entries and notes to owner in index
    Implement ownership in Entry model, and query the model filtered by
    current user id in the view. This ensures that the entries passed to the
    template are owned by the current user.
    The query to the Notes model is filtered by the ids of the entries
    belonging to the current user.

--- todo_lists/models.py
+++ todo_lists/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Entry(models.Model):
     heading = models.CharField(max_length=50)
     text = models.TextField(max_length=200, blank=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     class Meta:
         verbose_name_plural = "entries"
--- todo_lists/views.py
+++ todo_lists/views.py
@@ -5,8 +5,16 @@ from . import models
 
 def index(request):
     """Displays the index view o the Todo List app."""
-    entries = models.Entry.objects.all()
-    notes = models.Note.objects.all()
+    # Get entries belonging to current user.
+    entries = models.Entry.objects.filter(owner=request.user.id)
+
+    # Get all notes referencing an entry in entries as a flat list.
+    notes = []
+    for entry in entries:
+        query_set = models.Note.objects.filter(parent_entry__id=entry.id)
+        for obj in query_set:
+            notes.append(obj)
+
     context = {
         "entries": entries,
         "notes": notes,

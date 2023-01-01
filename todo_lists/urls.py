403dcc2a3289dbade81b58284ebfda00582af356
Max <teratoscincus@hotmail.com>
2023-01-01 12:44:05 +0100

    Add URLs, views and templates for ModelForms

--- todo_lists/urls.py
+++ todo_lists/urls.py
@@ -7,6 +7,6 @@ from . import views
 app_name = "todo_lists"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("new_entry", views.new_entry, name="new_entry"),
-    path("new_note", views.new_note, name="new_note"),
+    path("new_entry/", views.new_entry, name="new_entry"),
+    path("new_note/<int:parent_entry_id>/", views.new_note, name="new_note"),
 ]
--- todo_lists/views.py
+++ todo_lists/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 
 from . import models
+from . import forms
 
 
 def index(request):
@@ -20,3 +21,53 @@ def index(request):
         "notes": notes,
     }
     return render(request, "todo_lists/index.html", context)
+
+
+def new_entry(request):
+    if request.method == "POST":
+        # Populate form with data from request
+        form = forms.EntryForm(request.POST)
+
+        # Validate form and redirect user.
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.owner = request.user
+            new_entry.save()
+
+            return redirect("todo_lists:index")
+    else:
+        # Create blank form.
+        form = forms.EntryForm()
+
+    # Display blank or invalid form.
+    context = {
+        "form": form,
+    }
+    return render(request, "todo_lists/new_entry.html", context)
+
+
+def new_note(request, parent_entry_id):
+    # Init parent Entry instance.
+    entry = get_object_or_404(models.Entry, id=parent_entry_id)
+
+    if request.method == "POST":
+        # Populate form with data from request
+        form = forms.NoteForm(request.POST)
+
+        # Validate form and redirect user.
+        if form.is_valid():
+            new_note = form.save(commit=False)
+            new_note.parent_entry = entry
+            new_note.save()
+
+            return redirect("todo_lists:index")
+    else:
+        # Create blank form.
+        form = forms.NoteForm()
+
+    # Display blank or invalid form.
+    context = {
+        "form": form,
+        "entry": entry,
+    }
+    return render(request, "todo_lists/new_note.html", context)
